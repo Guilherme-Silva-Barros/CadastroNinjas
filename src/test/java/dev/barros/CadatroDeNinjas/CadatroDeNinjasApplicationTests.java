96ffe7cf442254bacb0f5f7a07d987bd273d1bde
Guilherme Barros <barrosgui2002@gmail.com>
2026-01-27 16:37:20 -0300

    first commit

---
+++ src/test/java/dev/barros/CadatroDeNinjas/CadatroDeNinjasApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.barros.CadatroDeNinjas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadatroDeNinjasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
