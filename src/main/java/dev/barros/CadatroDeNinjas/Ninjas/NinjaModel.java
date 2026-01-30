2999b3d14e5bf2475a544c7240fdffb2a45b1ab7
Guilherme Barros <barrosgui2002@gmail.com>
2026-01-29 21:09:09 -0300

    fix:Coloca a coluna de email como UNIQUE

--- src/main/java/dev/barros/CadatroDeNinjas/Ninjas/NinjaModel.java
+++ src/main/java/dev/barros/CadatroDeNinjas/Ninjas/NinjaModel.java
@@ -16,6 +16,7 @@ public class NinjaModel {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String nome;
+    @Column(unique = true)
     private String email;
     private int idade;
     @ManyToOne //um ninja vai ter somente uma missão
