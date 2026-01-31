1842d473dd1f9c4f43d18895c392e132c93d6e51
Guilherme Barros <barrosgui2002@gmail.com>
2026-01-30 21:31:46 -0300

    feat: adição do flyway e atualização no banco de dados

---
+++ src/main/resources/db/migrations/V2__Add_rank_tb_cadastro.sql
@@ -0,0 +1,7 @@
+-- V2: Migrations para adicionar a coluna de rank na tabela de cadastro --
+
+ALTER TABLE tb_cadastro
+ADD COLUMN rank VARCHAR(255);
+
+ALTER TABLE tb_cadastro
+ADD COLUMN rank VARCHAR(255);
